f1e37474745cceb42035b4bed453ae288141575a
Shishi-43 <otisdaniel020@gmail.com>
2025-07-27 21:18:22 +0000

    Prompt() can't be used in node.js scripts.

--- Function/recursion.js
+++ Function/recursion.js
@@ -1,11 +1,21 @@
-let number = prompt("Enter a postive number: ");
+// the prompt() can't be used in Node.js, so we use readline for input
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-function isEven(number){
-    if (number < 0){
-        console.log('Please enter a positive number');
-    }else if(number%2 ===0){
-        return true;
-    }else{
-        return false;
+readline.question('Enter a positive number: ', (number) => {
+    function isEven(number){
+        if (number < 0){
+            console.log('Please enter a positive number');
+        }else if(number === 0){
+            return true;
+        }else if(number === 1){
+            return false;
+        }else{
+            return isEven(number - 2);
+        }
     }
-}
+    console.log(isEven(number));
+    readline.close();
+});
